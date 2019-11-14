1eaaf4c3b8be45007940cbd8de502a0bfbfc8a78
zhibowan <zhibowan@zhibos-mbp.fios-router.home>
2019-11-13 22:11:43 -0500

    init commit

---
+++ src/algorithm/ShaEncryption.java
@@ -0,0 +1,48 @@
+package algorithm;
+
+import java.security.MessageDigest;
+import java.util.HashMap;
+
+public class ShaEncryption {
+	private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+	
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for (int j = 0; j < bytes.length; j++) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+	        hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+	    }
+	    return new String(hexChars).toLowerCase();
+	}
+	
+	public static String sha256(String base) {
+	    try{
+	        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+	        byte[] hash = digest.digest(base.getBytes("UTF-8"));
+	        return bytesToHex(hash);
+	    } catch(Exception ex){
+	       throw new RuntimeException(ex);
+	    }
+	}
+	
+	
+	
+	public static String sha256Decription(String code, HashMap<String, String> codeTable) {
+		if(codeTable.containsKey(code)) {
+			return codeTable.get(code);
+		}
+		else
+		{return null;}
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(ShaEncryption.sha256("foo"));
+
+	}
+
+}
---
+++ src/rpc/messages.java
@@ -0,0 +1,112 @@
+package rpc;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import algorithm.ShaEncryption;
+
+/**
+ * Servlet implementation class messages
+ */
+@WebServlet("/messages/*")
+public class messages extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public HashMap<String, String> codeTable = new HashMap<String, String>();
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public messages() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		String code = request.getPathInfo().substring(1);
+
+		String base = ShaEncryption.sha256Decription(code, codeTable);
+
+		if (base != null) {
+			String jsonString = "{\"message\":\"" + base + "\"}";
+
+			JSONObject respObj = null;
+			try {
+				respObj = new JSONObject(jsonString);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			RpcHelper.writeJsonObject(response, respObj);
+
+		}
+
+		else {
+
+			String jsonString = "{\"err_msg\":\"" + "Message not found" + "\"}";
+
+			JSONObject respObj = null;
+			try {
+				respObj = new JSONObject(jsonString);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			//response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			RpcHelper.writeJsonObject(response, respObj);
+			
+			//response.getWriter().append("Served at: ").append(code);
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		JSONObject reqObj = RpcHelper.readJsonObject(request);
+		String base = null;
+		try {
+			base = reqObj.getString("message");
+		} catch (JSONException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		String code = ShaEncryption.sha256(base);
+		this.codeTable.put(code, base);
+		String jsonString = "{\"digest\":\"" + code + "\"}";
+
+		JSONObject respObj = null;
+		try {
+			respObj = new JSONObject(jsonString);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		RpcHelper.writeJsonObject(response, respObj);
+
+	}
+
+}
